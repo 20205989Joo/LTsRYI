147a13d635023698201980c848a7f65da55f1ca1
JJoo <antiquebox@cau.ac.kr>
2024-05-14 13:48:28 +0900

    frontbackend1

--- server.js
+++ server.js
@@ -10,16 +10,22 @@ server
   .use(express.json())
   .use(cookieParser());
 
+let savedResults = []; // 데이터를 저장할 배열
+
 server.post('/api/saveResults', (req, res) => {
     console.log("POST /api/saveResults called");
     console.log("Request headers:", req.headers);
     console.log("Request body:", req.body);
+
+    const data = req.body.data;
+    savedResults.push(data);
+
     res.status(200).json({ message: 'Results saved successfully' });
 });
 
 server.get('/api/saveResults', (req, res) => {
     console.log("GET /api/saveResults called");
-    res.status(200).json({ message: 'GET request to saveResults endpoint successful' });
+    res.status(200).json(savedResults);
 });
 
 server.listen(PORT, () => {
